d76935f33f1be1b9ff2e9b6603c6b523ed9ea8a0
wdwdwq <qkenr7278@gmail.com>
2024-07-10 09:41:42 +0900

    게시글 수정 기능 구현, 글의 유무 체크 x

--- src/main/java/org/example/system/App.java
+++ src/main/java/org/example/system/App.java
@@ -9,9 +9,11 @@ import java.util.Scanner;
 
 public class App {
     private Scanner sc;
+    int lastId;
 
     public App(Scanner sc) {
         this.sc = sc;
+        lastId = 0;
     }
 
     public void run() {
@@ -23,7 +25,6 @@ public class App {
             String cmd = sc.nextLine().trim();
 
             if (cmd.equals("exit")) {
-                System.out.println("==Article exit==");
                 break;
 
             } else if (cmd.length() == 0) {
@@ -32,14 +33,15 @@ public class App {
             }
             if (cmd.equals("article write")) {
                 System.out.println("==글쓰기==");
-                String regDate = Util.getNow();
-                System.out.print("title : ");
+                int id = lastId + 1;
+                System.out.print("제목 : ");
                 String title = sc.nextLine();
-                System.out.print("body : ");
+                System.out.print("내용 : ");
                 String body = sc.nextLine();
 
-                System.out.println("게시글이 작성 되었습니다");
-
+                lastId++;
+                System.out.println(id + "번 글이 작성되었습니다");
+/////////////////////////////////////////////////////////////////////////////////////
                 Connection conn = null;
                 PreparedStatement pstmt = null;
 
@@ -69,21 +71,20 @@ public class App {
                     System.out.println("에러 : " + e);
                 } finally {
                     try {
-                        if (conn != null && !conn.isClosed()) {
-                            conn.close();
-                        }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    try {
                         if (pstmt != null && !pstmt.isClosed()) {
                             pstmt.close();
                         }
                     } catch (SQLException e) {
                         e.printStackTrace();
                     }
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
                 }
-                //
             } else if (cmd.equals("article list")) {
                 System.out.println("==목록==");
 
@@ -153,8 +154,73 @@ public class App {
                     System.out.printf("     %d      /    %s    \n", article.getId(), article.getTitle());
                 }
 
+            } else if (cmd.startsWith("article modify")) {
+
+                int id = 0;
+
+                try {
+                    id = Integer.parseInt(cmd.split(" ")[2]);
+                } catch (Exception e) {
+                    System.out.println("정수 입력");
+                    continue;
+                }
+
+                System.out.println("==글 수정==");
+                System.out.print("새 제목 : ");
+                String title = sc.nextLine();
+                System.out.print("새 내용 : ");
+                String body = sc.nextLine();
+
+                //////////////////////////////
+
+                Connection conn = null;
+                PreparedStatement pstmt = null;
+
+                try {
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    String url = "jdbc:mariadb://127.0.0.1:3306/AM_JDBC_2024_07?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                    conn = DriverManager.getConnection(url, "root", "");
+                    System.out.println("연결 성공!");
+
+                    String sql = "UPDATE article ";
+                    sql += "SET updateDate = NOW()";
+                    if (title.length() > 0) {
+                        sql += " , title = '" + title + "'";
+                    }
+                    if (body.length() > 0) {
+                        sql += " , `body` = '" + body + "'";
+                    }
+                    sql += " WHERE id = " + id + ";";
+
+                    System.out.println(sql);
+
+                    pstmt = conn.prepareStatement(sql);
+
+                    pstmt.executeUpdate();
+
+
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패" + e);
+                } catch (SQLException e) {
+                    System.out.println("에러 : " + e);
+                } finally {
+                    try {
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(id + "번 글이 수정되었습니다.");
             }
         }
-
     }
 }
