4e513658f432a62d10b496ef18df0e2aa37a65b8
wdwdwq <qkenr7278@gmail.com>
2024-07-10 10:03:24 +0900

    App 생성, 기능 내부의 Connection을 명령어 입력할 때마다로 변경

--- src/main/java/org/example/system/Main.java
+++ src/main/java/org/example/system/Main.java
@@ -1,12 +1,8 @@
 package org.example.system;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        new App(sc).run();
-        sc.close();
+        new App().run();
     }
 }
